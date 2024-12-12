30292133ce436c7633a7f262af72ecb092259148
kakakepi <viklybi1@gmail.com>
2024-12-12 23:38:42 +0300

    upd lab7: asd

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab6
 {
@@ -37,6 +38,7 @@ namespace Lab6
                 Console.WriteLine($"Инверсированная строка: {reversedString}");
             }
         }
+
         static void Task3()
         {
             Console.Write("Укажите название файла:");
@@ -46,7 +48,7 @@ namespace Lab6
                 Console.WriteLine("Имя файла не может быть пустым.");
                 return;
             }
-            string fileName = $"../../../{fileNamePath}";
+            string fileName = $"../../../txtfiles/{fileNamePath}";
             if (!File.Exists(fileName))
             {
                 Console.WriteLine("Файл не существует.");
@@ -56,7 +58,7 @@ namespace Lab6
             {
                 string content = File.ReadAllText(fileName);
                 string upperContent = content.ToUpper();
-                File.WriteAllText($"../../../TxtFile.txt", upperContent);
+                File.WriteAllText($"../../../txtfiles/TxtFile.txt", upperContent);
 
                 Console.WriteLine("Файл успешно создан с содержимым в верхнем регистре.");
             }
@@ -65,6 +67,29 @@ namespace Lab6
                 Console.WriteLine($"Произошла ошибка: {ex.Message}");
             }
         }
+        static void Task6()
+        {
+            Song song1 = new Song("Glamur", "nkeei");
+            Song song2 = new Song("Me against the world", "2pac", song1);
+            Song song3 = new Song("Money Trees", "Kendrick Lamar", song2);
+            Song song4 = new Song("I smoked away my brain", "A$AP Rocky",song3);
+            Song[] songs = { song1, song2, song3, song4 };
+            Console.WriteLine("Список песен:");
+            foreach (Song song in songs)
+            {
+                Console.WriteLine(song.Title() + "\n");
+            }
+            Console.Write("Сравнение первой и второй песни: ");
+            if (song1.Equals(song2))
+            {
+                Console.WriteLine("Первая и вторая песни одинаковы.");
+            }
+            else
+            {
+                Console.WriteLine("Первая и вторая песни разные.");
+            }
+        }
+
         static void Task4()
         {
             object obj1 = 88888888;
@@ -72,18 +97,61 @@ namespace Lab6
             CheckIFormattable(obj1);
             CheckIFormattable(obj2);
         }
+
+        static void Task5()
+        {
+            Console.Write("Введите имя входного файла: ");
+            string inputFilePath = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(inputFilePath))
+            {
+                Console.WriteLine("Имена файлов не могут быть пустыми.");
+                return;
+            }
+            string fileName = $"../../../txtfiles/{inputFilePath}";
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("Входной файл не существует.");
+                return;
+            }
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                using (StreamWriter writer = new StreamWriter($"../../../txtfiles/FileForTask5.txt"))
+                {
+                    foreach (string line in lines)
+                    {
+                        string email = line;
+                        SearchMail(ref email);
+                        if (!string.IsNullOrWhiteSpace(email))
+                        {
+                            writer.WriteLine(email);
+                        }
+                    }
+                }
+                Console.WriteLine("Файл с адресами электронной почты успешно создан.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+        
+        public static void SearchMail(ref string s)
+        {
+            s = s.Split("#")[1].Replace(" ", "");
+        }
+
         static void CheckIFormattable(object obj)
         {
-            if (obj is IFormattable)
+            if (obj is IFormattable formattableObj)
             {
-                IFormattable? formattableObj = obj as IFormattable;
                 Console.WriteLine("Объект реализует интерфейс IFormattable.");
-                Console.WriteLine($"Форматированное представление: {formattableObj?.ToString(null, null)}");
+                Console.WriteLine($"Форматированное представление: {formattableObj.ToString(null, null)}");
             }
             else
             {
                 Console.WriteLine("Объект не реализует интерфейс IFormattable.");
-        }
+            }
         }
 
         static void Main(string[] args)
@@ -92,6 +160,10 @@ namespace Lab6
             Task2();
             Task3();
             Task4();
+            Task5();
+            Task6();
         }
     }
 }
+
+
